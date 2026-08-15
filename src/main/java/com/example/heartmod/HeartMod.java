0c8b97dfeb82613249f41eb816a4d84de61027aa
mist717 <dylangrajeda21@gmail.com>
2026-08-15 16:06:24 -0700

    Update HeartMod.java

--- src/main/java/com/example/heartmod/HeartMod.java
+++ src/main/java/com/example/heartmod/HeartMod.java
@@ -70,7 +70,7 @@ public class HeartMod implements ModInitializer {
 
         private MutableText glyph() {
             return Text.literal(String.valueOf(glyphChar()))
-                    .setStyle(Style.EMPTY.withFont(new StyleSpriteSource.Font(FONT_ID)).withColor(color));
+                    .setStyle(Style.EMPTY.withFont(new StyleSpriteSource.Font(FONT_ID)) .withColor(color) .withShadowColor(0)); }
         }
 
         private MutableText spacer() {
